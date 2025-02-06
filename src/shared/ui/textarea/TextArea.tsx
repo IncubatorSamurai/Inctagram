3435b519c526d6a077a51e4e6fa39b48b39848ce
golzi99 <golzi99@gmail.com>
2025-02-07 00:00:27 +0300

    IC-7: feat: update styles and structure textarea

--- src/shared/ui/textarea/TextArea.tsx
+++ src/shared/ui/textarea/TextArea.tsx
@@ -1,5 +1,38 @@
-import React from 'react'
+import React, { ChangeEvent, ComponentPropsWithoutRef, forwardRef } from 'react'
+import { Typography } from '@/shared/ui/typography'
+import s from './TextArea.module.scss'
+import clsx from 'clsx'
 
-export const TextArea = () => {
-  return <div></div>
-}
+type Props = {
+  error?: string
+  id?: string
+  title?: string
+  onValueChange?: (value: string) => void
+  className?: string
+} & ComponentPropsWithoutRef<'textarea'>
+
+export const TextArea = forwardRef<HTMLTextAreaElement, Props>(
+  ({ id, error, title, className, onValueChange, onChange, ...props }, ref) => {
+    const onChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+      onValueChange?.(e.target.value)
+      onChange?.(e)
+    }
+    const textAreaStyle = clsx(s.textarea, className, error && s.textAreaError)
+
+    return (
+      <div className={s.container}>
+        {title && <Typography className={s.title}>{title}</Typography>}
+        <textarea
+          className={textAreaStyle}
+          onChange={onChangeHandler}
+          ref={ref}
+          id={id}
+          {...props}
+        ></textarea>
+        {error && <Typography variant={'error'}>{error}</Typography>}
+      </div>
+    )
+  }
+)
+
+TextArea.displayName = 'TextArea'
