8215e7450e4bd691d23dae1bb95d52e74908320d
Natallia <natalyasafarevich@gmail.com>
2025-02-10 12:16:15 +0100

    IC-11 fix: fix styles and tag

--- src/shared/ui/recaptcha/Recaptcha.tsx
+++ src/shared/ui/recaptcha/Recaptcha.tsx
@@ -8,13 +8,13 @@ type Props = { error?: string } & Partial<ReCAPTCHAProps>
 export const Recaptcha = ({ error, ...rest }: Props) => {
   if (!process.env.NEXT_PUBLIC_RECAPTCHA_KEY) return
   return (
-    <form className={clsx(s.container, error && s.errorContainer)}>
+    <div className={clsx(s.container, error && s.errorContainer)}>
       <ReCAPTCHA {...rest} sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_KEY} theme="dark" />
       {error && (
         <Typography variant="error" className={clsx(s.errorText)}>
           {error}
         </Typography>
       )}
-    </form>
+    </div>
   )
 }
