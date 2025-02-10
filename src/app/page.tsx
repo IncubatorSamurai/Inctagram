cd821ea65278d60355de33733a0010f4e8fbfe92
golzi99 <golzi99@gmail.com>
2025-02-10 23:37:12 +0300

    IC-21: fix: homePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import { Button } from '@/shared/ui/button'
-import { TrendingUpOutlineIcon } from '@/shared/assets/icons/TrendingUpOutlineIcon'
-
 export default function BaseHome() {
-  return (
-    <div>
-      BaseHome
-      <Button variant={'icon'}>
-        <TrendingUpOutlineIcon />
-      </Button>
-    </div>
-  )
+  return <div>BaseHome</div>
 }
