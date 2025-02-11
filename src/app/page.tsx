37d08d39858d96256b08383181a484adc8733aa6
fedjes <salvados78@mail.ru>
2025-02-11 11:11:45 +0300

    IC-12 added: create custom hook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+'use client'
+import { Pagination } from '@/shared/ui/pagination/Pagination'
+import {useState} from "react";
+
+
 export default function BaseHome() {
-  return <div>BaseHome</div>
+  const  [testV, setTestV] = useState(1)
+    const handlTest = (value:number) => {
+        console.log('valueTest: ', value)
+        setTestV(value)
+    }
+  return (
+    <div>
+      BaseHome
+      <Pagination
+        totalItemsCount={10} // Общее количество элементов
+        pageItemsCount={1} // Количество элементов на странице
+        currentPage={testV} // Текущая страница
+        siblingCount={1} // Количество соседних страниц (по умолчанию 1)
+        changeCurrentPage={handlTest}
+      ></Pagination>
+    </div>
+  )
 }
