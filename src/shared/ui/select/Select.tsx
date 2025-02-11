76cab3fd0653c52372cbf1317d408c4ac58ad776
fedjes <salvados78@mail.ru>
2025-02-11 13:40:18 +0300

    IC-16 added: svg icon

--- src/shared/ui/select/Select.tsx
+++ src/shared/ui/select/Select.tsx
@@ -1,57 +1,56 @@
 import * as React from 'react'
 import * as SelectRadix from '@radix-ui/react-select'
-import { ComponentPropsWithoutRef } from 'react'
-import { clsx } from 'clsx'
+import {ComponentPropsWithoutRef} from 'react'
+import {clsx} from 'clsx'
 import s from './Select.module.scss'
 
 
-import { SelectItem } from './selectItem/SelectItem'
-import {Typography} from "../typography/Typography";
+import {SelectItem} from './selectItem/SelectItem'
+import {Typography} from "@/shared/ui/typography";
+import {ArrowIosUpIcon} from "@/shared/assets/icons/ArrowIosUpIcon";
 
 
 type Options = {
-  id: string
-  label: string
+    id: string
+    label: string
 }
 
 type Props = {
-  disabled?: boolean
-  label?: string
-  placeholder?: string
-  options: Options[]
+    disabled?: boolean
+    label?: string
+    placeholder?: string
+    options: Options[]
 } & ComponentPropsWithoutRef<typeof SelectRadix.Root>
 
-export const SelectBox = ({ label, placeholder, options, disabled = false, ...props }: Props) => {
-  const classNames = {
-    selectGroup: clsx(s.selectGroup),
-    selectLabel: clsx(s.selectLabel, disabled && s.selectLabelDisabled),
-    selectTrigger: clsx(s.selectTrigger),
-  }
+export const SelectBox = ({label, placeholder, options, disabled = false, ...props}: Props) => {
+    const classNames = {
+        selectGroup: clsx(s.selectGroup),
+        selectLabel: clsx(s.selectLabel, disabled && s.selectLabelDisabled),
+        selectTrigger: clsx(s.selectTrigger),
+    }
 
-  return (
-    <div className={s.selectWrapper}>
-      <Typography className={classNames.selectLabel}>{label}</Typography>
-      <SelectRadix.Root disabled={disabled} {...props}>
-        <SelectRadix.Trigger className={classNames.selectTrigger}>
-          <SelectRadix.Value placeholder={placeholder} className={s.selectValue} />
-          <SelectRadix.Icon>{/*<ChevronDownIcon />*/}</SelectRadix.Icon>
-        </SelectRadix.Trigger>
-        <SelectRadix.Portal>
-          <SelectRadix.Content className={s.selectContent} position={'popper'}>
-            {/*<ChevronUpIcon />*/}
-            <SelectRadix.Viewport>
-              <SelectRadix.Group className={classNames.selectGroup}>
-                {options.map(el => (
-                  <SelectItem key={el.id} value={el.id}>
-                    <Typography className={s.typography}>{el.label}</Typography>
-                  </SelectItem>
-                ))}
-              </SelectRadix.Group>
-            </SelectRadix.Viewport>
-            {/* <ChevronDownIcon /> */}
-          </SelectRadix.Content>
-        </SelectRadix.Portal>
-      </SelectRadix.Root>
-    </div>
-  )
+    return (
+        <div className={s.selectWrapper}>
+            <Typography className={classNames.selectLabel}>{label}</Typography>
+            <SelectRadix.Root disabled={disabled} {...props}>
+                <SelectRadix.Trigger className={classNames.selectTrigger}>
+                    <SelectRadix.Value placeholder={placeholder} className={s.selectValue}/>
+                    <ArrowIosUpIcon color={'var(--color-light-100)'} className={s.arrow}></ArrowIosUpIcon>
+                </SelectRadix.Trigger>
+                <SelectRadix.Portal>
+                    <SelectRadix.Content className={s.selectContent} position={'popper'}>
+                        <SelectRadix.Viewport>
+                            <SelectRadix.Group className={classNames.selectGroup}>
+                                {options.map(el => (
+                                    <SelectItem key={el.id} value={el.id}>
+                                        <Typography className={s.typography}>{el.label}</Typography>
+                                    </SelectItem>
+                                ))}
+                            </SelectRadix.Group>
+                        </SelectRadix.Viewport>
+                    </SelectRadix.Content>
+                </SelectRadix.Portal>
+            </SelectRadix.Root>
+        </div>
+    )
 }
