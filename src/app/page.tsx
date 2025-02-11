5d3b1bcc07ef15353200b2b740c95129662ec54e
fedjes <salvados78@mail.ru>
2025-02-11 13:43:11 +0300

    IC-16 done: done select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import {SelectBox} from "@/shared/ui/select";
-
 export default function BaseHome() {
-  return <div>BaseHome
-  <SelectBox disabled={false} options={[{id: '1', label: 'html'}, {id: '2', label: 'css'}, {id: '3', label: 'javascript'}]}></SelectBox>
-  </div>
+  return <div>BaseHome</div>
 }
