10a8c46509769a802be4b890480b98739a378570
fedjes <salvados78@mail.ru>
2025-02-18 17:06:56 +0300

    done pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,3 @@
-'use client'
-import { Pagination } from '@/shared/ui/pagination/Pagination'
-import { useState } from "react";
-
-
 export default function BaseHome() {
-  const [currentPage, setCurrentPage] = useState(1)
-  const [itemPerPage, setItemPerPage] = useState(10)
-  
-    const testHandlerChangeCurrentPage = (value: number) => {
-        setCurrentPage(value)
-        console.log(value);
-
-    }
-
-    const testHandlerChangeItemsPerPage = (value: number) => {
-        console.log(value);
-        setItemPerPage(value)
-    }
-
-    const requestTotalPageCount = 120 //начиная с 8 страниц появляются точки
-
-   
-    return (
-        <div>
-            BaseHome
-            <Pagination
-                changeCurrentPage={testHandlerChangeCurrentPage}
-                changeItemsPerPage={testHandlerChangeItemsPerPage}
-                currentPage={currentPage}
-                totalCount={requestTotalPageCount}
-                neighbours={1}
-                pageSize={itemPerPage}>
-            </Pagination>
-        </div>
-    )
-}
+    return <>Base Home</>
+  }
