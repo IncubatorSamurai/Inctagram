ae7f383d5dfa6d18a0820692df88dad9d7ab346e
fedjes <salvados78@mail.ru>
2025-02-18 16:30:38 +0300

    IC-12: added pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,37 @@
 'use client'
-import {Pagination} from '@/shared/ui/pagination/Pagination'
-import {useState} from "react";
+import { Pagination } from '@/shared/ui/pagination/Pagination'
+import { useState } from "react";
 
 
 export default function BaseHome() {
-    const [testV, setTestV] = useState(1)
-    const handlTest = (value: number) => {
-        console.log('valueTest: ', value)
-        setTestV(value)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [itemPerPage, setItemPerPage] = useState(10)
+  
+    const testHandlerChangeCurrentPage = (value: number) => {
+        setCurrentPage(value)
+        console.log(value);
+
     }
+
+    const testHandlerChangeItemsPerPage = (value: number) => {
+        console.log(value);
+        setItemPerPage(value)
+    }
+
+    const requestTotalPageCount = 120 //начиная с 8 страниц появляются точки
+
+   
     return (
         <div>
             BaseHome
             <Pagination
-                totalItemsCount={10} // Общее количество элементов
-                pageItemsCount={1} // Количество элементов на странице
-                currentPage={testV} // Текущая страница
-                siblingCount={1} // Количество соседних страниц (по умолчанию 1)
-                changeCurrentPage={handlTest}
-            ></Pagination>
+                changeCurrentPage={testHandlerChangeCurrentPage}
+                changeItemsPerPage={testHandlerChangeItemsPerPage}
+                currentPage={currentPage}
+                totalCount={requestTotalPageCount}
+                neighbours={1}
+                pageSize={itemPerPage}>
+            </Pagination>
         </div>
     )
 }
--- src/shared/ui/pagination/Pagination.tsx
+++ src/shared/ui/pagination/Pagination.tsx
@@ -1,39 +1,40 @@
 'use client'
-import {Typography} from '@/shared/ui/typography'
-import {Button} from '@/shared/ui/button'
-import {usePagination, DOTS} from '@/shared/ui/pagination/hooks/usePagination'
-import {useEffect} from 'react'
+import { ArrowIosBackIcon } from '@/shared/assets/icons/ArrowIosBackIcon'
 import s from './Pagination.module.scss'
+import { ArrowIosForwardIcon } from '@/shared/assets/icons/ArrowIosForwardIcon'
+import { Typography } from './../typography/Typography';
+import { SelectBox } from '../select';
+import { useEffect } from 'react';
+import { clsx } from 'clsx'
+import { DOTS, usePagination } from './hook/usePagination';
+
 
 type Props = {
     changeCurrentPage: (value: number) => void
-    // changeItemsPerPage: (value: number) => void
-    className?: string
+    changeItemsPerPage: (value: number) => void
     currentPage: number
-    pageItemsCount: number
-    siblingCount?: number
-    totalItemsCount: number
+    pageSize: number
+    neighbours?: number
+    totalCount: number
+
 }
 
-export const Pagination = (props: Props) => {
+export const Pagination = ({ changeCurrentPage,
+    changeItemsPerPage,
+    currentPage,
+    pageSize,
+    neighbours,
+    totalCount,
+}: Props) => {
 
-    const {
-        changeCurrentPage,
-        // changeItemsPerPage,
-        currentPage,
-        pageItemsCount,
-        siblingCount,
-        totalItemsCount,
-    } = props
-    // const onChangeValue = (value: string) => {
-    //   changeItemsPerPage(Number(value))
-    // }
-    const paginationRange = usePagination({
-        currentPage,
-        pageItemsCount,
-        siblingCount,
-        totalItemsCount,
-    })
+    const options = [{ id: '1', label: '10' }, { id: '2', label: '20' }, { id: '3', label: '30' }, { id: '4', label: '50' }, { id: '5', label: '100' },]
+
+    const onChangeValue = (selectedId: string) => {
+        const selectedOption = options.find(option => option.id === selectedId)
+        changeItemsPerPage(Number(selectedOption?.label))
+    }
+
+    const paginationRange = usePagination({ currentPage, pageSize, neighbours, totalCount })
     const lastPage = paginationRange?.[paginationRange.length - 1]
     const firstPage = 1
 
@@ -47,34 +48,55 @@ export const Pagination = (props: Props) => {
     }
     const onNext = () => {
         changeCurrentPage(currentPage + 1)
-        console.log(currentPage + 1)
     }
 
     const onPrevious = () => {
-        console.log(currentPage)
         changeCurrentPage(currentPage - 1)
-        console.log(currentPage - 1)
+    }
+
+    const classNames = {
+        arrowBackward: clsx(s.arrow, currentPage === firstPage && s.disabled),
+        arrowForward: clsx(s.arrow, s.rotateArrow, currentPage === lastPage && s.disabled),
+        paginationContainer: clsx(s.pagination_container),
     }
 
     return (
-        <div>
-            {/*//TODO change button on arrow and add className prev*/}
-            <Button onClick={onPrevious}/>
+        <div className={classNames.paginationContainer}>
+            <button onClick={onPrevious} disabled={currentPage === firstPage} className={s.arrow}>
+                <ArrowIosBackIcon />
+            </button>
 
             {paginationRange?.map((pageNumber, index) => {
                 if (pageNumber === DOTS) {
-                    return DOTS
+                    return <div key={index} className={s.dots}>...</div>
                 }
 
                 return (
-                    <li className={s.test} key={index} onClick={() => changeCurrentPage(Number(pageNumber))}>
+                    <li
+                        tabIndex={0}
+                        className={clsx(s.pagination_item, pageNumber === currentPage && s.selected)}
+                        key={index}
+                        onClick={() => changeCurrentPage(Number(pageNumber))}
+                    >
                         {pageNumber}
                     </li>
                 )
             })}
-            {/*//TODO change button on arrow and add className next*/}
-            <Button onClick={onNext}/>
-            <Typography>Показать Select на странице</Typography>
+            <button onClick={onNext} disabled={currentPage === lastPage} className={s.arrow}>
+                <ArrowIosForwardIcon />
+            </button>
+            <Typography className={s.selectWrapper}>
+                Показать
+            </Typography>
+            <SelectBox
+                defaultValue={options[0].id}
+                onValueChange={onChangeValue}
+                options={options}
+            >
+            </SelectBox>
+            <Typography className={s.selectWrapper}>
+                на странице
+            </Typography>
         </div>
     )
 }
