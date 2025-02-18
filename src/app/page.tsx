69f8082dc358f58de8c4563ad61cf2d5a00bac95
veronikachernobai <bombeze13@gmail.com>
2025-02-18 15:21:05 +0300

    add Header,Sidebar,Dropdown, refactor Select, refactor Storybook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,3 @@
-'use client'
-import { Header } from '@/widgets/header'
-
-import { Sidebar } from '@/widgets/sidebar/Sidebar'
-
 export default function BaseHome() {
-  return (
-      <>
-        <Header headerTitle={'Inctagram'} isAuth={true} isAdmin={true} />
-        <div style={{ display: 'flex ', alignItems: 'flex-start' }}>
-          <Sidebar isAdmin={false} />
-        </div>
-      </>
-  )
+  return <>Base Home</>
 }
