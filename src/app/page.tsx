0df0392ab4ad7923adc2a246ad50c33a3e5dc4ee
fedjes <salvados78@mail.ru>
2025-02-19 10:45:55 +0300

    fix: styles border pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+'use client'
+import { Pagination } from "@/shared/ui/pagination";
+
 export default function BaseHome() {
-    return <>Base Home</>
+  const test = (value:number) => {
+    console.log()
+  }
+  const test2 = (value:number) => {
+    console.log()
+  }
+
+    return <>Base Home
+      <Pagination 
+      changeCurrentPage={test} 
+      changeItemsPerPage={test2} 
+      currentPage={1} 
+      pageSize={10} 
+      totalCount={100} 
+      neighbours={1}>
+
+      </Pagination>
+    </>
   }
