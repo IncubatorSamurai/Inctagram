4d016ad5e4d0b17a6e8da2add516039306006893
fedjes <salvados78@mail.ru>
2025-02-19 10:49:58 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,3 @@
-'use client'
-import { Pagination } from "@/shared/ui/pagination";
-
 export default function BaseHome() {
-  const test = (value:number) => {
-    console.log()
-  }
-  const test2 = (value:number) => {
-    console.log()
-  }
-
-    return <>Base Home
-      <Pagination 
-      changeCurrentPage={test} 
-      changeItemsPerPage={test2} 
-      currentPage={1} 
-      pageSize={10} 
-      totalCount={100} 
-      neighbours={1}>
-
-      </Pagination>
-    </>
+    return <>Base Home</>
   }
