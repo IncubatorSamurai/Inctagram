f47f06eaecaa872a4ff69b3523fa44500fded489
Natallia <natalyasafarevich@gmail.com>
2025-02-20 18:39:49 +0100

    IC-25 feat: delete some files

--- src/pageComponents/signIn/ui/SignIn.tsx
+++ src/pageComponents/signIn/ui/SignIn.tsx
@@ -1,12 +1,10 @@
 import { Typography } from '@/shared/ui/typography'
 import s from './SignIn.module.scss'
-import { AuthWidget } from '@/widgets/authWidget'
 
 export const SignInPage = () => {
   return (
     <div className={s.container}>
       <Typography variant="h1">Sign In</Typography>
-      <AuthWidget />
     </div>
   )
 }
