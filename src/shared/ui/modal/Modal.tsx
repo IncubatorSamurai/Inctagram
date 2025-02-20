730a0602e326c43c5109b172915e5da5a3442058
golzi99 <golzi99@gmail.com>
2025-02-20 16:32:45 +0300

    IC-30 feat: update Modal window

--- src/shared/ui/modal/Modal.tsx
+++ src/shared/ui/modal/Modal.tsx
@@ -3,6 +3,7 @@ import { CloseIcon } from '../../assets/icons/CloseIcon'
 import { clsx } from 'clsx'
 import * as DialogRadix from '@radix-ui/react-dialog'
 import s from './Modal.module.scss'
+import { Typography } from '@/shared/ui/typography'
 
 type Props = {
   children?: ReactNode
@@ -11,10 +12,12 @@ type Props = {
   trigger?: ReactNode
 } & ComponentPropsWithoutRef<typeof DialogRadix.Root>
 
-export const Modal = ({ children, className, title, trigger, ...props }: Props) => {
+const DialogClose = DialogRadix.Close
+
+const Modal = ({ children, className, title, trigger, ...props }: Props) => {
   return (
     <DialogRadix.Root {...props}>
-      {trigger && <DialogRadix.Trigger>{trigger}</DialogRadix.Trigger>}
+      {trigger && <DialogRadix.Trigger asChild>{trigger}</DialogRadix.Trigger>}
       <DialogRadix.Portal>
         <DialogRadix.Overlay className={s.DialogOverlay} />
         <DialogRadix.Content
@@ -22,9 +25,13 @@ export const Modal = ({ children, className, title, trigger, ...props }: Props) 
         >
           {title ? (
             <div className={s.header}>
-              <DialogRadix.Title className={s.DialogTitle}>{title}</DialogRadix.Title>
-              <DialogRadix.Close aria-label={'Close'}>
-                <CloseIcon className={s.IconButton} />
+              <DialogRadix.Title className={s.DialogTitle}>
+                <Typography variant={'h1'}>{title}</Typography>
+              </DialogRadix.Title>
+              <DialogRadix.Close asChild>
+                <button aria-label={'Close'} className={s.IconButton}>
+                  <CloseIcon />
+                </button>
               </DialogRadix.Close>
             </div>
           ) : (
@@ -32,11 +39,14 @@ export const Modal = ({ children, className, title, trigger, ...props }: Props) 
               <CloseIcon />
             </DialogRadix.Close>
           )}
-          <div className={clsx(title && s.contentContainer)}>
-            <div>{children}</div>
-          </div>
+          <div className={clsx(title && s.contentContainer)}>{children}</div>
         </DialogRadix.Content>
       </DialogRadix.Portal>
     </DialogRadix.Root>
   )
 }
+
+DialogClose.displayName = DialogRadix.Close.displayName
+Modal.displayName = 'Modal'
+
+export { Modal, DialogClose }
