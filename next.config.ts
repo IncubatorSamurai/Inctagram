a5649d9ae15d69fd7d4d7e7f35415ccab4570114
golzi99 <golzi99@gmail.com>
2025-02-22 19:53:12 +0300

    IC-36 feat: add structure of next-intl

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from 'next'
+import createNextIntlPlugin from 'next-intl/plugin'
 
-const nextConfig: NextConfig = {
-  i18n: {
-    locales: ['en', 'ru'],
-    defaultLocale: 'ru',
-  },
-}
+const withNextIntl = createNextIntlPlugin()
 
-export default nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig: NextConfig = {}
+
+export default withNextIntl(nextConfig)
