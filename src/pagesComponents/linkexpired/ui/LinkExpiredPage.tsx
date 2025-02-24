067117dd412c775a1fd9abd373eddafc90f214b6
Natallia <natalyasafarevich@gmail.com>
2025-02-24 14:04:41 +0100

    IC-32 feat: finish desktop

--- src/pagesComponents/linkexpired/ui/LinkExpiredPage.tsx
+++ src/pagesComponents/linkexpired/ui/LinkExpiredPage.tsx
@@ -1,15 +1,25 @@
 import { Typography } from '@/shared/ui/typography'
 import s from './LinkExpiredPage.module.scss'
 import { LinkExpiredForm } from '@/features/auth/linkExpiredForm'
+import Image from 'next/image'
 
 export const LinkExpiredPage = () => {
   return (
     <div className={s.container}>
-      <Typography variant="h1">Email verification link expired</Typography>
-      <Typography variant="regular_text_16">
-        Looks like the verification link has expired. Not to worry, we can send the link again
-      </Typography>
+      <div className={s.box}>
+        <Typography variant="h1">Email verification link expired</Typography>
+        <Typography className={s.text} variant="regular_text_16">
+          Looks like the verification link has expired. Not to worry, we can send the link again
+        </Typography>
+      </div>
       <LinkExpiredForm />
+      <Image
+        className={s.image}
+        src={'/link-expired.svg'}
+        height={100}
+        width={100}
+        alt="link expired"
+      />
     </div>
   )
 }
