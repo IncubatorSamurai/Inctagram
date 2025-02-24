a3c2e51bfdfac6447715d65b3811afc5a6ddcdbe
Natallia <natalyasafarevich@gmail.com>
2025-02-24 13:53:01 +0100

    IC-32 feat: add input & button in form

--- src/features/auth/linkExpiredForm/ui/LinkExpiredForm.tsx
+++ src/features/auth/linkExpiredForm/ui/LinkExpiredForm.tsx
@@ -1,5 +1,12 @@
+import { Input } from '@/shared/ui/input'
 import s from './LinkExpiredForm.module.scss'
+import { Button } from '@/shared/ui/button'
 
 export const LinkExpiredForm = () => {
-  return <form className={s.form} action=""></form>
+  return (
+    <form className={s.form} action="">
+      <Input label="Email" />
+      <Button className={s.button}>Resend verification link</Button>
+    </form>
+  )
 }
