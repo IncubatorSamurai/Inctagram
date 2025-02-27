aa4003175beae35ff5a202d2e88a389141c8a759
Natallia <natalyasafarevich@gmail.com>
2025-02-27 16:34:07 +0100

    IC-35 feat: add form

--- src/features/auth/signIn/ui/SignInForm.tsx
+++ src/features/auth/signIn/ui/SignInForm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
 import { Button } from '@/shared/ui/button'
 import { Input } from '@/shared/ui/input'
@@ -8,9 +9,10 @@ import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { SignInSchema, SignInSchemaData } from '../model/types'
 import { useLoginMutation } from '@/shared/api/auth/authApi'
+import { useEffect } from 'react'
 
 export const SignInForm = () => {
-  const [login] = useLoginMutation()
+  const [login, { error }] = useLoginMutation()
 
   const { register, handleSubmit, formState } = useForm<SignInSchemaData>({
     mode: 'onBlur',
@@ -21,11 +23,33 @@ export const SignInForm = () => {
   const onSubmit = (data: SignInSchemaData) => {
     login({ email: data.email, password: data.password })
   }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  console.log((error as any)?.data?.messages)
+  useEffect(() => {
+    fetch('https://inctagram.work/api/v1/auth/registration', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userName: 'tester',
+        email: 'natalliasafarevich@gmail.com',
+        password: 'Exd4mple!',
+        baseUrl: 'http://localhost:3000/en/signin',
+      }),
+    }).then(a => console.log(a))
+    // {
+    //   "userName": "string",
+    //   "email": "string",
+    //   "password": "Ex4mple!",
+    //   "baseUrl": "http://localhost:3000"
+    // }
+  }, [])
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <Input
         {...register('email')}
-        error={errors?.email?.message}
+        error={errors?.email?.message || (error as any)?.data?.messages}
         className={s.input}
         label="Email"
         placeholder="Epam@epam.com"
