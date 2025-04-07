37bee54f4855498e899c9a4d13857d0dde3d1299
golzi99 <golzi99@gmail.com>
2025-04-07 16:58:33 +0300

    DEV fix: take out requests ssr into utils

--- src/app/[locale]/(public)/users/[userId]/page.tsx
+++ src/app/[locale]/(public)/users/[userId]/page.tsx
@@ -1,25 +1,5 @@
 import { ProfilePage } from '@/entities/profile/ui/Profile'
-
-async function getPublicData(userId: string) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}v1/public-user/profile/${userId}`, {
-    cache: 'no-store',
-  })
-  return res.json()
-}
-
-type PostsProps = {
-  userId: string
-}
-
-async function getPublicPosts({ userId }: PostsProps) {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}v1/public-posts/user/${userId}/,?pageSize=8`,
-    {
-      cache: 'no-store',
-    }
-  )
-  return res.json()
-}
+import { getPublicData, getPublicPosts } from '@/shared/utils'
 
 type Props = {
   params: Promise<{ userId: string }>
@@ -29,7 +9,7 @@ export default async function UserProfile({ params }: Props) {
   const userId = (await params).userId
   const [resPublicData, resPublicPosts] = await Promise.all([
     getPublicData(userId),
-    getPublicPosts({ userId }),
+    getPublicPosts(userId),
   ])
   return <ProfilePage resPublicData={resPublicData} resPublicPosts={resPublicPosts} />
 }
