fe465dd9367939f6d5bcb27000eeb82ef3d9d6e8
golzi99 <golzi99@gmail.com>
2025-04-13 17:25:39 +0300

    IC-65 feat: add sub costs

--- src/features/settings/accountManegement/ui/AccountTypes.tsx
+++ src/features/settings/accountManegement/ui/AccountTypes.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import {Typography} from "@/shared/ui/typography";
-import {useTranslations} from "next-intl";
-import {Card} from "@/shared/ui/card";
+import React, { useState } from 'react'
+import { Typography } from '@/shared/ui/typography'
+import { useTranslations } from 'next-intl'
+import { Card } from '@/shared/ui/card'
 import s from './styles.module.scss'
-import {RadioGroups} from "@/shared/ui/radio-groups";
+import { RadioGroups } from '@/shared/ui/radio-groups'
 
 export const AccountTypes = () => {
-    const t = useTranslations('profile.profileSettingsTabs')
-    const options = [
-        {value: 'Personal', label: 'Personal', id: '0'},
-        {value: 'Business', label: 'Business', id: '1'}
-    ]
+  const t = useTranslations('profile.profileSettingsTabs')
+  const accountTypes = [
+    { value: 'personal', label: 'Personal', id: 'personal' },
+    { value: 'business', label: 'Business', id: 'business' },
+  ]
+  const costs = [
+    { value: '10per1', label: '$10 per 1 Day', id: '10per1' },
+    { value: '50per7', label: '$50 per 7 Day', id: '50per7' },
+    { value: '100perMonth', label: '$100 per month', id: '100perMonth' },
+  ]
+  const [accountState, setAccountState] = useState(accountTypes[0].value)
+  const [subCost, setSubCost] = useState(costs[0].value)
 
-    return (
-        <div>
-            <div>
-                <Typography variant={'h3'}>{t('accountType')}:</Typography>
-                <Card className={s.accountTypeContainer}>
-                    <RadioGroups options={options} className={s.radioGroup} defaultValue={'Personal'}/>
-                </Card>
-            </div>
-        </div>
-
-    );
-};
+  return (
+    <div>
+      <div>
+        <Typography variant={'h3'}>{t('accountType')}</Typography>
+        <Card className={s.accountTypeContainer}>
+          <RadioGroups
+            options={accountTypes}
+            className={s.radioGroup}
+            defaultValue={accountState}
+            onValueChange={type => {
+              console.log(type)
+              setAccountState(type)
+            }}
+          />
+        </Card>
+        {accountState === accountTypes[1].value && (
+          <>
+            <Typography variant={'h3'}>{t('cost')}</Typography>
+            <Card className={s.accountTypeContainer}>
+              <RadioGroups
+                options={costs}
+                className={s.radioGroup}
+                defaultValue={subCost}
+                onValueChange={cost => setSubCost(cost)}
+              />
+            </Card>
+          </>
+        )}
+      </div>
+    </div>
+  )
+}
