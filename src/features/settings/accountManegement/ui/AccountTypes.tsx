409dd6ddfb27be5f1cdc88243db97f30fd74b51d
golzi99 <golzi99@gmail.com>
2025-04-14 14:38:54 +0300

    IC-65 feat: add disabled one item into RadioGroup, add Current Subs

--- src/features/settings/accountManegement/ui/AccountTypes.tsx
+++ src/features/settings/accountManegement/ui/AccountTypes.tsx
@@ -4,11 +4,14 @@ import { useTranslations } from 'next-intl'
 import { Card } from '@/shared/ui/card'
 import s from './styles.module.scss'
 import { RadioGroups } from '@/shared/ui/radio-groups'
+import { Checkbox } from '@/shared/ui/checkbox'
 
 export const AccountTypes = () => {
   const t = useTranslations('profile.profileSettingsTabs')
+  const [activeSubs, setActiveSubs] = useState(true)
+  const [autoRenewal, setAutoRenewal] = useState(true)
   const accountTypes = [
-    { value: 'personal', label: 'Personal', id: 'personal' },
+    { value: 'personal', label: 'Personal', id: 'personal', disabled: activeSubs },
     { value: 'business', label: 'Business', id: 'business' },
   ]
   const costs = [
@@ -21,38 +24,57 @@ export const AccountTypes = () => {
 
   return (
     <div>
-      <div>
-        <Typography variant={'h3'}>{t('accountType')}</Typography>
-        <Card className={s.accountTypeContainer}>
-          <RadioGroups
-            options={accountTypes}
-            className={s.radioGroup}
-            defaultValue={accountState}
-            onValueChange={type => {
-              console.log(type)
-              setAccountState(type)
-            }}
-          />
-        </Card>
-        {accountState === accountTypes[1].value && (
-          <>
-            <Typography variant={'h3'}>{t('cost')}</Typography>
-            <Card className={s.accountCostContainer}>
-              <RadioGroups
-                options={costs}
-                className={s.radioGroup}
-                defaultValue={subCost}
-                onValueChange={cost => setSubCost(cost)}
-              />
-            </Card>
-            <div className={s.paymentSystemsContainer}>
-              <Card className={s.payment}>PayPal</Card>
-              or
-              <Card className={s.payment}>Strip</Card>
+      {activeSubs && (
+        <>
+          <Typography variant={'h3'}>{t('currentSubs')}</Typography>
+          <Card className={s.subsContainer}>
+            <div className={s.expire}>
+              <Typography variant={'medium_text_14'}>{t('expireAt')}</Typography>
+              <Typography variant={'bold_text_14'}>12.02.2022</Typography>
             </div>
-          </>
-        )}
-      </div>
+            <div className={s.nextPayment}>
+              <Typography variant={'medium_text_14'}>{t('nextPayment')}</Typography>
+              <Typography variant={'bold_text_14'}>13.02.2022</Typography>
+            </div>
+          </Card>
+          <Checkbox
+            checked={autoRenewal}
+            onChange={setAutoRenewal}
+            id={'autoRenewal'}
+            label={t('autoRenewal')}
+            className={s.autoRenewal}
+          />
+        </>
+      )}
+      <Typography variant={'h3'}>{t('accountType')}</Typography>
+      <Card className={s.accountTypeContainer}>
+        <RadioGroups
+          options={accountTypes}
+          className={s.radioGroup}
+          defaultValue={accountState}
+          onValueChange={type => {
+            setAccountState(type)
+          }}
+        />
+      </Card>
+      {accountState === accountTypes[1].value && (
+        <>
+          <Typography variant={'h3'}>{t('cost')}</Typography>
+          <Card className={s.accountCostContainer}>
+            <RadioGroups
+              options={costs}
+              className={s.radioGroup}
+              defaultValue={subCost}
+              onValueChange={cost => setSubCost(cost)}
+            />
+          </Card>
+          <div className={s.paymentSystemsContainer}>
+            <Card className={s.payment}>PayPal</Card>
+            or
+            <Card className={s.payment}>Strip</Card>
+          </div>
+        </>
+      )}
     </div>
   )
 }
