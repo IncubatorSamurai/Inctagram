fed853bf7b7eeb09612c2cffc91867bd780d5883
veronikachernobai <bombeze13@gmail.com>
2025-04-22 12:50:28 +0300

    IC-fix modal

--- src/app/[locale]/profile/[userId]/@postModal/page.tsx
+++ src/app/[locale]/profile/[userId]/@postModal/page.tsx
@@ -1,8 +1,17 @@
 // import { PostModalGlobal } from '@/entities/postModal'
-import { PublicModalAlex } from '@/features/post/PostModal'
+import { PostsModal } from '@/features/post/PostModal'
+import getPublicPost from '@/shared/api/post/serverRequests/getPublicPost'
+import getComments from '@/shared/api/post/serverRequests/getPostComments'
 
 export default async function PostModal({ searchParams }: { searchParams: { postId: string } }) {
-  const postId = (await searchParams).postId
+  const resolvedSearchParams = await searchParams
+  const postId = resolvedSearchParams?.postId ? Number(resolvedSearchParams.postId) : null
+  if (!postId) {
+    return
+  }
 
-  return <PublicModalAlex postId={+postId} />
+  const post = await getPublicPost({ id: postId })
+  const commentsData = await getComments({ id: postId })
+
+  return <PostsModal post={post} commentsData={commentsData} postId={postId} />
 }
--- src/features/post/PostModal/index.ts
+++ src/features/post/PostModal/index.ts
@@ -1,1 +1,1 @@
-export * from './ui/PostModal'
+export * from './ui/PostsModal'
