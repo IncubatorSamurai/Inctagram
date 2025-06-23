2ff2e193a7b0265e8eadb802da65601e7d6b5c7d
Natallia <natalyasafarevich@gmail.com>
2025-06-23 19:06:45 +0200

    IC-120 refactor: delete data

--- src/widgets/deviceTab/ui/DeviceTab.tsx
+++ src/widgets/deviceTab/ui/DeviceTab.tsx
@@ -1,10 +1,7 @@
-import { useGetAllDevicesQuery } from '@/shared/api/device/deviceApi'
 import { LogOutAllDevice, LogOutDevice } from '@/features/logOutDevice'
 
 export const DeviceTab = () => {
-  const { data } = useGetAllDevicesQuery({})
 
-  console.log(data)
   return (
     <div>
       <LogOutAllDevice />
